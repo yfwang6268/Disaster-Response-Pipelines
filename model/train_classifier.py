2921e58338d4af290d89f4898d8efc315c7d3112
Ethan Wang <xawangyifei@hotmail.com>
2019-08-18 17:39:48 -0500

    correct dump model code

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -101,7 +101,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
-    pickle.dumps(model, model_filepath)
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
