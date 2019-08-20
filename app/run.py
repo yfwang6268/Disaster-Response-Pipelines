c3bc41236f3184c0a062c8422e49ba5778adba62
Ethan Wang <xawangyifei@hotmail.com>
2019-08-19 20:46:03 -0500

    Update run.py

--- app/run.py
+++ app/run.py
@@ -6,7 +6,7 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request, json
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
